11dba7da8b09699c4bc828e957eebd6df72b9c1d
AliMoradpour <Mr.mrdp@yahoo.com>
2023-04-13 00:22:40 +0430

    initial project

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,23 @@
+const SignUpForm = () => {
+    return ( 
+            <div>
+                <form action="">
+                    <div className="formControl">
+                        <label>Name</label>
+                        <input type="text" />
+                    </div>
+                    <div className="formControl">
+                        <label>Email</label>
+                        <input type="text" />
+                    </div>
+                    <div className="formControl">
+                        <label>Password</label>
+                        <input type="text" />
+                    </div>
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+}
+ 
+export default SignUpForm;
