a55f9dcfb61c2121c3cb19fe15a20c8bf7ee5dbb
AliMoradpour <Mr.mrdp@yahoo.com>
2023-04-13 00:56:50 +0430

    add changeHandler and submitHandler on form

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,23 +1,54 @@
+import { useState } from "react";
+
 const SignUpForm = () => {
-    return ( 
-            <div>
-                <form action="">
-                    <div className="formControl">
-                        <label>Name</label>
-                        <input type="text" />
-                    </div>
-                    <div className="formControl">
-                        <label>Email</label>
-                        <input type="text" />
-                    </div>
-                    <div className="formControl">
-                        <label>Password</label>
-                        <input type="text" />
-                    </div>
-                    <button>Submit</button>
-                </form>
-            </div>
-        );
-}
- 
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = ({ target }) => {
+    setUserData({ ...userData, [target.name]: target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefualt();
+  };
+  
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="formControl">
+          <label>Name</label>
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={userData.name}
+            name="name"
+          />
+        </div>
+        <div className="formControl">
+          <label>Email</label>
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={userData.email}
+            name="email"
+          />
+        </div>
+        <div className="formControl">
+          <label>Password</label>
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={userData.password}
+            name="password"
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
 export default SignUpForm;
