5e516373cd6b7347b598437c70f5f815ede39b87
AliMoradpour <Mr.mrdp@yahoo.com>
2023-04-13 10:54:50 +0430

    add formik validation

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,13 +4,25 @@ const initialValues = {
   email: "",
   password: "",
 };
+
+const onSubmit = (values) => {
+  console.log(values);
+};
+
+const validate = (values) => {
+  let errors = {};
+
+  if (!values.name) errors.name = "Name is Required";
+  if (!values.email) errors.email = "Email is Required";
+  if (!values.password) errors.password = "Password is Required";
+  return errors;
+};
+
 const SignUpForm = () => {
-  const onSubmit = (values) => {
-    console.log(values);
-  };
   const formik = useFormik({
     initialValues,
     onSubmit,
+    validate,
   });
 
   return (
@@ -21,27 +33,39 @@ const SignUpForm = () => {
           <input
             type="text"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.name}
             name="name"
           />
+          {formik.errors.name && formik.touched.name && (
+            <span className="error">{formik.errors.name}</span>
+          )}
         </div>
         <div className="formControl">
           <label>Email</label>
           <input
             type="text"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.email}
             name="email"
           />
+          {formik.errors.email && formik.touched.email && (
+            <span className="error">{formik.errors.email}</span>
+          )}
         </div>
         <div className="formControl">
           <label>Password</label>
           <input
             type="text"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.password}
             name="password"
           />
+          {formik.errors.password && formik.touched.password && (
+            <span className="error">{formik.errors.password}</span>
+          )}
         </div>
         <button type="submit">Submit</button>
       </form>
