8eca2decab361b26e358b86006cee29c123b6636
AliMoradpour <Mr.mrdp@yahoo.com>
2023-04-13 11:28:25 +0430

    add password confirmation and phone number , increase password security

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,23 +1,39 @@
 import { useFormik } from "formik";
-import * as yup from "yup";
+import * as Yup from "yup";
 
 const initialValues = {
   name: "",
   email: "",
+  number: "",
   password: "",
+  passwordConfirm: "",
 };
 
 const onSubmit = (values) => {
   console.log(values);
 };
 
-const validationSchema = yup.object({
-  name: yup.string().required("Name is Required"),
-  email: yup
-    .string()
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .required("Name is Required")
+    .min(5, "Must be more than 5 char"),
+  email: Yup.string()
     .email("Invalid email format")
     .required("Email is Required"),
-  password: yup.string().required("Password is Required"),
+  number: Yup.string()
+    .required("Phone Number is Required")
+    .matches(/^[0-9]{11}$/, "Invalid Phone Number")
+    .nullable(),
+  password: Yup.string()
+    .required("Password is Required")
+    .min(8, "must be more than 8 char")
+    .matches(
+      /^.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?].**$/,
+      "Need one special character"
+    ),
+  passwordConfirm: Yup.string()
+    .required("Password Confirmation is Required")
+    .oneOf([Yup.ref("password"), null], "Password must match"),
 });
 
 const SignUpForm = () => {
@@ -45,14 +61,36 @@ const SignUpForm = () => {
           )}
         </div>
         <div className="formControl">
-          <label>Password</label>
+          <label>Phone Number</label>
           <input
             type="text"
+            {...formik.getFieldProps("number")}
+            name="number"
+          />
+          {formik.errors.number && formik.touched.number && (
+            <span className="error">{formik.errors.number}</span>
+          )}
+        </div>
+        <div className="formControl">
+          <label>Password</label>
+          <input
+            type="password"
             {...formik.getFieldProps("password")}
             name="password"
           />
           {formik.errors.password && formik.touched.password && (
             <span className="error">{formik.errors.password}</span>
+          )}
+        </div>
+        <div className="formControl">
+          <label>Password Conformation</label>
+          <input
+            type="password"
+            {...formik.getFieldProps("passwordConfirm")}
+            name="passwordConfirm"
+          />
+          {formik.errors.passwordConfirm && formik.touched.passwordConfirm && (
+            <span className="error">{formik.errors.passwordConfirm}</span>
           )}
         </div>
         <button type="submit">Submit</button>
